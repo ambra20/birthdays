db88c27d11b97fc41cdbddf7c2ea226236d66e94
ambra20 <150253061+ambra20@users.noreply.github.com>
2023-11-29 15:13:12 +0100

    Create test_birthday.py

---
+++ test_birthday.py
@@ -0,0 +1,6 @@
+def test_smoke_valid_input():
+# Your smoke test code here for the function 'print_birthdays_str'
+def test_error_entry():
+# Your error entry test code here for 'return_birthday'
+def test_corner_case():
+# Your corner case test code here for 'return_birthday'
